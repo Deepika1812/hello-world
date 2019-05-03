c0d1d9645019d98c15483f8ae0ee34de07edecb5
Deepika1812 <44048442+Deepika1812@users.noreply.github.com>
2019-05-03 11:41:13 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -11,9 +11,3 @@ element=wait.until(EC.presence_of_element_located((By.CLASS_NAME,'_2zrpKA')))
 element.send_keys("9515129203")
 browser.find_element_by_xpath("//input[@type=\"password\"]").send_keys("Deepu5a6")
 browser.find_element_by_xpath("//button[@class='_2AkmmA _1LctnI _7UHT_c']").click()
-"""def search_items(str):
-    wait2=WebDriverWait(browser,10)
-    element2=wait2.until(EC.presence_of_element_located((By.CLASS_NAME,'LM6RPg')))
-    element2.send_keys(str)
-    element2.click()
-search_items("televisions")"""
